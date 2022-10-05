60511b04b45caecddbd37771e8c18c909d0fa5cd
Eblackk <aps14blacerig@gmail.com>
2022-10-05 19:52:18 -0400

    Updated dogHouseTest

--- src/test/java/com/github/curriculeon/DogHouseTest.java
+++ src/test/java/com/github/curriculeon/DogHouseTest.java
@@ -2,7 +2,10 @@ package com.github.curriculeon;
 
 import com.github.curriculeon.animals.Dog;
 import com.github.curriculeon.animals.animal_creation.AnimalFactory;
+import com.github.curriculeon.animals.animal_storage.CatHouse;
 import com.github.curriculeon.animals.animal_storage.DogHouse;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Date;
@@ -16,6 +19,59 @@ public class DogHouseTest {
     // TODO - Create tests for `void remove(Dog dog)`
     // TODO - Create tests for `Dog getDogById(Integer id)`
     // TODO - Create tests for `Integer getNumberOfDogs()`
+
+    @Before
+    public void setup() {
+        DogHouse.clear();
+    }
+
+    @Test
+    public void getNumberOfDogs() {
+        //Given
+        Integer expectedNumberOfDogs = DogHouse.getNumberOfDogs();
+        Dog expectedDog = new Dog(null, null, null);
+
+        //When
+        DogHouse.add(expectedDog);
+        Integer actualNumberOfDogS = DogHouse.getNumberOfDogs();
+        expectedNumberOfDogs = expectedNumberOfDogs + 1;
+
+
+        //Then
+        Assert.assertEquals(expectedNumberOfDogs, actualNumberOfDogS);
+    }
+
+
+    @Test
+    public void removeDogByIdTest() {
+        //Given
+        Integer expectedId = 0;
+        Dog expectedDog = new Dog(null, null, expectedId);
+        DogHouse.add(expectedDog);
+        //When
+        DogHouse.remove(expectedId);
+        Dog actualDog = DogHouse.getDogById(expectedId);
+
+
+        //Then
+        Assert.assertNull(actualDog);
+    }
+
+    @Test
+    public void addDogByIdTest() {
+        //Given
+        Integer expectedId = 0;
+        Dog expectedDog = new Dog(null, null, expectedId);
+
+
+        //When
+        DogHouse.add(expectedDog);
+        Dog actualDog = DogHouse.getDogById(expectedId);
+
+
+        //Then
+        Assert.assertEquals(expectedDog, actualDog);
+    }
 
     @Test
     public void testGetNumberOfDogs() {
